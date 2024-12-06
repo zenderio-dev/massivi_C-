c5b07c775b49d053bf3f07e0961b1a8e2d736265
Макс <149411166+zenderio-dev@users.noreply.github.com>
2024-12-06 11:45:09 +0800

    Add files via upload

---
+++ task_4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    int SIZE; 
+
+    cout << "Введите размер массива через пробел: " << endl;
+    cin >> SIZE; // Размер массива
+
+    // Динамическое выделение памяти под массив
+    int* array = new int[SIZE]; 
+    
+    // Заполнение массива
+    cout << "Введите элементы массива: " << endl;
+    for (int i = 0; i < SIZE; ++i) {
+        cin >> array[i]; 
+    }
+
+    int max_length = 0; // Максимальная длина последовательности нулей
+    int current_length = 0; // Текущая длина последовательности нулей
+
+    for (int i = 0; i < SIZE; ++i) {
+        if (array[i] == 0) { // Если текущий элемент массива равен нулю
+            current_length++; // Прибавляем к значению текущей длины массива единицу
+        }
+        else {
+            current_length = 0; // Сбрасываем значение длины
+        }
+        
+        if (current_length > max_length) { // Если текущая длина больше максимальной
+            max_length = current_length;
+        }
+    }
+
+    cout << "Максимальная последовательность идущих подряд нулей = " << max_length << endl;
+
+    delete[] array; // Чистим память после выделения
+
+    return 0;
+}
